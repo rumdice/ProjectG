5a8d4b2a658a8da9e5998045a9c7bf6f6dea5fb1
rumdice <rumdice@gmail.com>
2021-12-04 09:13:51 +0900

    1. 프로트콜 별도 프로젝트로 분리 2. 접속 포트 변경 (7293 -> 5001) 접속 테스트 확인 완료 3. 7293 포트로 못 붙는 이유? 찾는 중

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,23 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-// https://grpc.io/docs/languages/csharp/quickstart/ 참조
-
-using Grpc.Core;
+﻿using Grpc.Core;
 using Grpc.Net.Client;
+using GrpcGreeter;
 using System;
 
 
-Channel channel = new Channel("127.0.0.1:7293", ChannelCredentials.Insecure);
 
-//var client = new Greeter.GreeterClient(channel);
-//String user = "you";
 
-//var reply = client.SayHello(new HelloRequest { Name = user });
-//Console.WriteLine("Greeting: " + reply.Message);
+//using var channel = GrpcChannel.ForAddress("https://localhost:7293")
 
-//var secondReply = client.SayHelloAgain(new HelloRequest { Name = user });
-//Console.WriteLine("Greeting: " + secondReply.Message);
-
-channel.ShutdownAsync().Wait();
+using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
--- GrpcServer/Services/GreeterService.cs
+++ GrpcServer/Services/GreeterService.cs
@@ -1,4 +1,5 @@
 using Grpc.Core;
+using GrpcGreeter;
 using GrpcServer;
 
 namespace GrpcServer.Services
