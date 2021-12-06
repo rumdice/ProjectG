66eb35703264a6f5848be741b68cbf96429ba7d5
rumdice <rumdice@gmail.com>
2021-12-06 14:31:32 +0900

    csv helper 사용 준비

--- GrpcServer/Services/LoginService.cs
+++ GrpcServer/Services/LoginService.cs
@@ -1,7 +1,9 @@
-﻿using Grpc.Core;
+﻿using CsvHelper;
+using Grpc.Core;
 using GrpcProtocol;
 using GrpcServer.DB;
 using MySql.Data.MySqlClient;
+using System.Globalization;
 
 namespace GrpcServer.Services
 {
@@ -11,6 +13,15 @@ namespace GrpcServer.Services
         // 로거 인스턴스 생성
         // TODO: 전역에서 단 한번만 선언하고 여러곳에서 사용하게끔
         private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        // csv temp class
+        public class Item
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public int Type { get; set; }
+            public int Count { get; set; }
+        }
 
         public override Task<LoginReply> LoginRpc(LoginRequest request, ServerCallContext context)
         {
@@ -49,7 +60,7 @@ namespace GrpcServer.Services
                 }
             }
 
-            // 3-1 redis
+            // 3-1. redis
             // TODO: 서버 초기 실행시 Init 하는 것으로 이동
             // 레디스 셋팅 및 사용 예시 
             var redis = new Redis();
@@ -58,7 +69,8 @@ namespace GrpcServer.Services
             var uuidKey = Guid.NewGuid().ToString();
             redis.SetString(uuidKey, "100");
 
-            // logging
+
+            // 4. logging
             Logger.Debug("Debug log");
             Logger.Info("Debug log");
             Logger.Warn("Debug log");
@@ -68,9 +80,18 @@ namespace GrpcServer.Services
             Logger.Warn("Debug log {0}", logMsg);
 
 
+            // 5.csv 
+            // TODO: 임시 테이블 구조를 객체 형태로 파싱
+            // 쿼리 수준으로 사용하기 좋은 클래스 제공 필요
+            var tablePath = "../Table/test.csv";
+            using (var reader = new StreamReader(tablePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Item>();
+            }
 
 
-            // 4.response
+            // 6.response
             var responseMessage = $"Welcome userid:{request.UserId} username:{request.UserName}";
             return Task.FromResult(new LoginReply
             {
